f80bba2a61e0f342f34e7606234890300481904d
Vibhanshu17 <vibhanshu1729@gmail.com>
2021-12-19 10:43:11 +0000

    added code to problem, solver, etc.

---
+++ src/solver.py
@@ -0,0 +1,41 @@
+from qubo_helper import Qubo
+from vrp_problem import VRPProblem
+from vrp_solution import VRPSolution
+from itertools import product
+import DWaveSolvers
+import networkx as nx
+import numpy as np
+from queue import Queue
+
+# Abstract class for VRP solvers.
+class VRPSolver:
+    # Attributes : VRPProblem
+    def __init__(self, problem):
+        self.problem = problem
+
+    def set_problem(self, problem):
+        self.problem = problem
+
+    # only_one_const - const in qubo making solution correct
+    # order_const - multiplier for costs in qubo
+    # It is recommended to set order_const = 1 and only_one_const
+    # big enough to make solutions correct. Bigger than sum of all
+    # costs should be enough.
+    def solve(self, only_one_const, order_const, solver_type = 'cpu'):
+        pass
+
+# Solver solves VRP only by QUBO formulation.
+class FullQuboSolver(VRPSolver):
+    def solve(self, only_one_const, order_const, solver_type = 'cpu'):
+        qubo = self.problem.get_full_qubo(only_one_const, order_const)
+        sample = DWaveSolvers.solve_qubo(qubo, solver_type = solver_type)
+        solution = VRPSolution(self.problem, sample)
+        return solution
+
+class CQMSolver(VRPSolver):
+    def solve(self, )
+        qubo = self.problem.get_full_qubo(only_one_const, order_const)
+        sample = DWaveSolvers.CQMSolver(cqm, label='cqm-vrptw')
+        solution = VRPSolution(self.problem, sample)
+
+        return solution
